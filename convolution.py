a1d41965db9adaac1a102a6887960f96ef7871ba
patrickmesana <patrick.mesana@gmail.com>
2017-04-24 08:53:57 -0400

    Just a test : I tried a conv without up/downsampling -> did not work. Also added Early stopping

--- convolution.py
+++ convolution.py
@@ -1,6 +1,7 @@
 # if you want to run this on GPU
 #THEANO_FLAGS="device=gpu,floatX=float32" ENV\Scripts\python.exe autoencoder.py
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Activation, BatchNormalization
+from keras.callbacks import EarlyStopping
 from keras.models import Model
 from keras import regularizers
 import numpy as np
@@ -9,29 +10,26 @@ import metrics
 
 
 input_img = Input(shape=(64, 64, 3))
-x = Conv2D(32, (3, 3), padding='same')(input_img)
+x = Conv2D(8, (3, 3), padding='same')(input_img)
 x = BatchNormalization()(x)
 x = Activation('relu')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(16, (3, 3), padding='same')(x)
+x = Conv2D(16, (3, 3), padding='same')(input_img)
 x = BatchNormalization()(x)
-x = Activation('relu')(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
-
+encoded = Activation('relu')(x)
 x = Conv2D(16, (3, 3), padding='same')(encoded)
 x = BatchNormalization()(x)
 x = Activation('relu')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(32, (3, 3), padding='same')(x)
+x = Conv2D(8, (3, 3), padding='same')(x)
 x = BatchNormalization()(x)
 x = Activation('relu')(x)
-x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
 
 # this model maps an input to its reconstruction
 autoencoder = Model(input_img, decoded)
 
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+autoencoder.summary()
 
 x_train_input = rs.read_images_from_pkl('training_input.pkl')
 x_train_target = rs.read_images_from_pkl('training_target_full.pkl')
@@ -46,11 +44,13 @@ x_train_target = x_train_target.reshape((len(x_train_target), 64, 64, 3))
 x_test_input = x_test_input.reshape((len(x_test_input), 64, 64, 3))
 x_test_target = x_test_target.reshape((len(x_test_target), 64, 64, 3))
 
+early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 history = autoencoder.fit(x_train_input, x_train_target,
                 epochs=30,
                 batch_size=256,
                 shuffle=True,
-                validation_data=(x_test_input, x_test_target))
+                validation_data=(x_test_input, x_test_target),
+                callbacks=[early_stopping])
 
 #writing predictions to disk
 decoded_imgs = autoencoder.predict(x_test_input)
