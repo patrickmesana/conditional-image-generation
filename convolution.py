76a79b6abdab72670adfd855b8dc830d34ac2823
patrickmesana <patrick.mesana@gmail.com>
2017-04-25 11:17:59 -0400

    Conv v1 with batchnormalization

--- convolution.py
+++ convolution.py
@@ -11,31 +11,36 @@ import metrics
 
 input_img = Input(shape=(64, 64, 3))
 
-
 x = Conv2D(16, (3, 3), padding='same')(input_img)
+x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
 x = MaxPooling2D((2, 2), padding='same')(x)
 
 x = Conv2D(32, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
 x = MaxPooling2D((2, 2), padding='same')(x)
 
 x = Conv2D(64, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
 x = Conv2D(64, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
 x = UpSampling2D((2, 2))(x)
 
 x = Conv2D(32, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
 x = UpSampling2D((2, 2))(x)
 
 x = Conv2D(16, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
 decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
